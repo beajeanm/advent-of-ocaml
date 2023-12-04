65a4ed335807f0d40b4d80e321c8d85a5aace58f
Jean-Michel Bea <jean-michel@opengamma.com>
2023-12-04 13:12:57 +0000

    Solve day 4 part 2

--- day_04/solution_04.ml
+++ day_04/solution_04.ml
@@ -11,29 +11,37 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 |}
   |> String.trim
 
+type cards = { winning_numbers : int list; numbers_you_have : int list }
+
 let parser =
   let open Angstrom in
   let open Util.Parser in
   let prefix = string "Card" <* ws <* integer <* char ':' <* ws in
   let numbers = sep_by ws integer in
-  prefix *> both (numbers <* string " |" <* ws) numbers
+  let+ winning_numbers, numbers_you_have =
+    prefix *> both (numbers <* string " |" <* ws) numbers
+  in
+  { winning_numbers; numbers_you_have }
+
+let parse_input input =
+  let lines = String.lines input in
+  let parse_line line =
+    Angstrom.parse_string ~consume:All parser line |> Result.get_exn
+  in
+  List.map ~f:parse_line lines
 
 module Set = Set.Make (Int)
 
-let filter_winnings (winning_numbers, numbers_you_have) =
+let winning_numbers card =
   let winning_numbers =
-    List.fold_left ~f:(Fun.flip Set.add) ~init:Set.empty winning_numbers
+    List.fold_left ~f:(Fun.flip Set.add) ~init:Set.empty card.winning_numbers
   in
-  List.filter ~f:(Fun.flip Set.mem @@ winning_numbers) numbers_you_have
+  List.filter ~f:(Fun.flip Set.mem @@ winning_numbers) card.numbers_you_have
 
 module Part_1 = struct
   let solve input =
-    let parse line =
-      Angstrom.parse_string ~consume:All parser line |> Result.get_exn
-    in
-    let lines = String.lines input in
-    let cards = List.map ~f:parse lines in
-    let winning_by_card = List.map ~f:filter_winnings cards in
+    let cards = parse_input input in
+    let winning_by_card = List.map ~f:winning_numbers cards in
     let score_by_card =
       List.filter ~f:(Fun.negate List.is_empty) winning_by_card
       |> List.map ~f:(fun l -> Int.pow 2 (List.length l - 1))
@@ -44,8 +52,23 @@ module Part_1 = struct
 end
 
 module Part_2 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let increment_number_cards cards_count current_index current_wins =
+    Iter.(current_index + 1 -- (current_index + current_wins))
+    |> Iter.iter (fun i ->
+           cards_count.(i) <- cards_count.(i) + cards_count.(current_index))
+
+  let solve input =
+    let cards = parse_input input in
+    let card_winnings =
+      List.map ~f:(fun card -> winning_numbers card |> List.length) cards
+      |> Array.of_list
+    in
+    (* We start with 1 card per line *)
+    let cards_count = Array.init (List.length cards) ~f:(Fun.const 1) in
+    Array.iteri ~f:(increment_number_cards cards_count) card_winnings;
+    Util.sum @@ Array.to_list cards_count
+
+  let%test "sample data" = Test.(run int (solve sample) ~expect:30)
 end
 
 let run_1 () =
@@ -53,5 +76,5 @@ let run_1 () =
   ()
 
 let run_2 () =
-  (* Run.solve_int (module Part_2); *)
+  Run.solve_int (module Part_2);
   ()
