aa1ed606e295f655a5c77e9bf008d7a860d4d5c5
Jean-Michel Bea <jean-michel@opengamma.com>
2023-12-04 15:19:08 +0000

    Reformat COMPLETED file

--- completed_days.ml
+++ completed_days.ml
@@ -10,11 +10,12 @@ let result_for_day day =
   let path i = Printf.sprintf "day_%02d/p%d.expected" day i in
   let result =
     match (file_is_not_empty (path 1), file_is_not_empty (path 2)) with
-    | true, false -> "A: *\tB: -"
-    | true, true -> "A: *\tB: *"
-    | _ -> "A: -\tB: -"
+    | true, false -> "A: 🟢\tB: 🔴"
+    | true, true -> "A: 🟢\tB: 🟢"
+    | _ -> "A: 🔴\tB: 🔴"
   in
-  Printf.sprintf "%i:\t%s" day result
+  Printf.sprintf "%i.\t%s" day result
 
 let () =
-  List.(1 -- 25 >|= result_for_day) |> String.concat ~sep:"\n" |> print_endline
+  List.(1 -- 25 >|= result_for_day) |> String.concat ~sep:"\n" |> print_endline;
+  print_endline ""
