f9a1ae6b96d018f261ee46e845c3380937b89e31
Jean-Michel Bea <jean-michel@opengamma.com>
2023-12-04 02:46:13 +0000

    Tidy up

--- day_03/solution_03.ml
+++ day_03/solution_03.ml
@@ -1,4 +1,5 @@
 open ContainersLabels
+module Group = Re.Group
 
 let sample =
   {|
@@ -20,42 +21,51 @@ let star_re = Re.(compile @@ char '*')
 let symbol_re = Re.(compile @@ diff any (alt [ char '.'; digit ]))
 let ( == ) = Int.equal
 
-module Part_1 = struct
-  let is_neighbour (group_row, group) (row, col) =
-    let start = Re.Group.start group 0 in
-    let stop = Re.Group.stop group 0 in
-    (row == group_row && col == start - 1)
-    || (row == group_row && col == stop)
-    || (row == group_row - 1 && col >= start - 1 && col <= stop)
-    || (row == group_row + 1 && col >= start - 1 && col <= stop)
+let numbers_from_groups groups =
+  List.map ~f:snd groups
+  |> List.map ~f:(fun group -> Group.get group 0)
+  |> List.map ~f:int_of_string
 
+(** Check if a number associated with its line number is a neighbour of a symbol represented by a dot *)
+let is_neighbour (num_row, num) (row, col) =
+  let num_col1 = Group.start num 0 in
+  let num_col2 = Group.stop num 0 - 1 in
+  abs (row - num_row) <= 1
+  && ((col >= num_col1 && col <= num_col2)
+     || col == num_col1 - 1
+     || col == num_col2 + 1)
+
+(** Parse the input and return its numbers and the symbols according to symbol_re *)
+let numbers_and_symbol input symbol_re =
+  let lines = String.lines input in
+  let numbers =
+    List.flat_map_i
+      ~f:(fun row line ->
+        Re.all number_re line |> List.map ~f:(fun num -> (row, num)))
+      lines
+  in
+  let symbols =
+    List.flat_map_i
+      ~f:(fun row line ->
+        Re.all symbol_re line |> List.map ~f:(fun group -> (row, group)))
+      lines
+  in
+  (numbers, symbols)
+
+module Part_1 = struct
+  (** Check if this number has at least 1 neighbour *)
   let has_a_neighbour symbols number =
-    Seq.filter (fun point -> is_neighbour number point) symbols
-    |> Seq.is_empty |> not
+    List.filter ~f:(fun point -> is_neighbour number point) symbols
+    |> List.is_empty |> not
 
   let solve input =
-    let lines = String.lines input in
-    let numbers =
-      List.flat_map_i
-        ~f:(fun row line ->
-          Re.all number_re line |> List.map ~f:(fun num -> (row, num)))
-        lines
-    in
-    let symbols =
-      List.flat_map_i
-        ~f:(fun row line ->
-          Re.all symbol_re line |> List.map ~f:(fun group -> (row, group)))
-        lines
-    in
+    let numbers, symbols = numbers_and_symbol input symbol_re in
     let symbol_coordinates =
-      List.map ~f:(fun (row, group) -> (row, Re.Group.start group 0)) symbols
-      |> Seq.of_list
+      List.map ~f:(fun (row, group) -> (row, Group.start group 0)) symbols
     in
     let valid_numbers =
       List.filter ~f:(has_a_neighbour symbol_coordinates) numbers
-      |> List.map ~f:snd
-      |> List.map ~f:(fun group -> Re.Group.get group 0)
-      |> List.map ~f:int_of_string
+      |> numbers_from_groups
     in
     List.fold_left ~f:Int.add ~init:0 valid_numbers
 
@@ -63,54 +73,24 @@ module Part_1 = struct
 end
 
 module Part_2 = struct
-  let is_neighbour (row, col) (num_row, num_col1, num_col2) =
-    abs (row - num_row) <= 1
-    && ((col >= num_col1 && col <= num_col2)
-       || col == num_col1 - 1
-       || col == num_col2 + 1)
-
-  let two_neigbours (star_row, star) numbers =
-    let star_col = Re.Group.start star 0 in
+  (** Return the number adjacent of a symbol, if the symbols has exactly 2 adjacent numbers.
+      Return the empty list otherwise. *)
+  let two_neighbours (star_row, star) numbers =
+    let star_col = Group.start star 0 in
     let neighbours =
-      List.filter
-        ~f:(fun (row, group) ->
-          is_neighbour (star_row, star_col)
-            (row, Re.Group.start group 0, Re.Group.stop group 0 - 1))
-        numbers
+      List.filter ~f:(Fun.flip is_neighbour (star_row, star_col)) numbers
     in
-    (* Format.printf "New Star: (%i, %i)\n" star_row star_col; *)
-    (* Format.printf "neighbours: %i\n" (List.length neighbours); *)
-    (* List.iter *)
-    (*   ~f:(fun (i, g) -> *)
-    (*     Format.printf "Num: (%i, %i, %i)\n" i (Re.Group.start g 0) *)
-    (*       (Re.Group.stop g 0)) *)
-    (*   numbers; *)
     if List.length neighbours == 2 then neighbours else []
 
   let solve input =
-    let lines = String.lines input in
-    let numbers =
-      List.flat_map_i
-        ~f:(fun row line ->
-          Re.all number_re line |> List.map ~f:(fun num -> (row, num)))
-        lines
-    in
-    let stars =
-      List.flat_map_i
-        ~f:(fun row line ->
-          Re.all star_re line |> List.map ~f:(fun group -> (row, group)))
-        lines
-    in
+    let numbers, stars = numbers_and_symbol input star_re in
     let valid_numbers =
       List.map
-        ~f:(fun star ->
-          two_neigbours star numbers |> List.map ~f:snd
-          |> List.map ~f:(fun group -> Re.Group.get group 0)
-          |> List.map ~f:int_of_string)
+        ~f:(fun star -> two_neighbours star numbers |> numbers_from_groups)
         stars
+      (* We need to remove the empty lists to avoid adding 1 for each empty list to the sum*)
       |> List.filter ~f:(Fun.negate List.is_empty)
     in
-    (* Format.printf "Valid numbers: %a" (List.pp @@ List.pp Int.pp) valid_numbers; *)
     List.map ~f:(List.fold_left ~f:Int.mul ~init:1) valid_numbers
     |> List.fold_left ~f:Int.add ~init:0
 
