90b4df944ae1d1758677bc46b9629a2f34a6846b
Jean-Michel Bea <jean-michel@opengamma.com>
2023-12-08 15:46:01 +0000

    Solve day 8 part 2

--- day_08/solution_08.ml
+++ day_08/solution_08.ml
@@ -38,7 +38,7 @@ XXX = (XXX, XXX)
   |}
   |> String.trim
 
-type direction = Right | Left | NoDirection
+type direction = Right | Left | NoDirection [@@deriving eq]
 
 module StringMap = Map.Make (String)
 
@@ -88,13 +88,48 @@ module Part_1 = struct
     let directions, mapping = parse input in
     go_to_zzz directions mapping
 
-  let%test "sample data" = Test.(run int (solve sample) ~expect:2)
-  let%test "sample data" = Test.(run int (solve sample2) ~expect:6)
+  let%test "sample data 1" = Test.(run int (solve sample) ~expect:2)
+  let%test "sample data 2" = Test.(run int (solve sample2) ~expect:6)
 end
 
 module Part_2 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample3) ~expect:6)
+  let end_with c s = Char.(String.get s 2 = c)
+
+  let go_to_zs directions (mapping : (string * string) StringMap.t) =
+    let move direction position =
+      let left, right =
+        StringMap.get position mapping |> Option.get_exn_or "Missing mapping"
+      in
+      match direction with
+      | Right -> right
+      | Left -> left
+      | _ -> failwith "Invalid direction"
+    in
+    let rec aux count position =
+      if end_with 'Z' position then count
+      else
+        let next_direction = RingBuffer.take_front_exn directions in
+        RingBuffer.push_back directions next_direction;
+        aux (count + 1) (move next_direction position)
+    in
+    let starting_positions =
+      StringMap.keys mapping
+      |> Iter.filter (fun s -> Char.(String.get s 2 = 'A'))
+      |> Iter.to_list
+    in
+    List.map ~f:(aux 0) starting_positions
+
+  let rec gcd u v = if v <> 0 then gcd v (u mod v) else abs u
+
+  let lcm m n =
+    match (m, n) with 0, _ | _, 0 -> 0 | m, n -> abs (m * n) / gcd m n
+
+  let solve input =
+    let directions, mappings = parse input in
+    let counts = go_to_zs directions mappings in
+    List.fold_left ~f:lcm ~init:(List.hd counts) counts
+
+  let%test "sample data 3" = Test.(run int (solve sample3) ~expect:6)
 end
 
 let run_1 () =
@@ -102,5 +137,5 @@ let run_1 () =
   ()
 
 let run_2 () =
-  (* Run.solve_int (module Part_2); *)
+  Run.solve_int (module Part_2);
   ()
