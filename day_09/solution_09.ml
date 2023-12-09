8938bf1b71d1a8c72a2a10aba1eb993c8b9f1bff
Jean-Michel Bea <jean-michel@opengamma.com>
2023-12-09 14:19:08 +0000

    Solve day 9 part 1

--- day_09/solution_09.ml
+++ day_09/solution_09.ml
@@ -1,12 +1,45 @@
 open ContainersLabels
+open Seq.Infix
 
 let sample = {|
-...
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
 |} |> String.trim
 
+let parse input =
+  let parse_line line =
+    String.split_on_char ~by:' ' line
+    |> List.map ~f:int_of_string |> Array.of_list
+  in
+  String.lines input |> List.map ~f:parse_line
+
+let gen_next_row row line =
+  let length = Array.length line in
+  let next_line = Array.init length ~f:(Fun.const 0) in
+  row -- (length - 1)
+  |> Seq.iter (fun i -> next_line.(i) <- line.(i) - line.(i - 1));
+  next_line
+
+let gen_all line =
+  let rec aux prev index acc =
+    if Array.for_all ~f:(fun a -> a = 0) prev then acc
+    else
+      let next = gen_next_row index prev in
+      aux next (index + 1) (next :: acc)
+  in
+  aux line 1 [ line ] |> List.rev
+
 module Part_1 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let solve input =
+    let parsed = parse input in
+    let gens = List.map ~f:gen_all parsed in
+    List.flat_map
+      ~f:(fun l -> List.map ~f:(fun a -> a.(Array.length a - 1)) l)
+      gens
+    |> Util.sum
+
+  let%test "sample data" = Test.(run int (solve sample) ~expect:114)
 end
 
 module Part_2 = struct
@@ -15,7 +48,7 @@ module Part_2 = struct
 end
 
 let run_1 () =
-  (* Run.solve_int (module Part_1); *)
+  Run.solve_int (module Part_1);
   ()
 
 let run_2 () =
