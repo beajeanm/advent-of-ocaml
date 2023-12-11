82263d75a4e35cdcfda2ef4f78d215fd608b826a
Jean-Michel Bea <jean-michel@opengamma.com>
2023-12-11 13:52:22 +0000

    Solve day 11 part 2

--- day_11/solution_11.ml
+++ day_11/solution_11.ml
@@ -33,48 +33,18 @@ let expanded_universe =
 |}
   |> String.trim
 
+type universe = {
+  empty_col : int list;
+  empty_row : int list;
+  planets : (int * int) list;
+}
+
 let parse input =
   String.lines_seq input
   |> Seq.map (fun line -> String.to_seq line |> Seq.to_array)
   |> Seq.to_array
 
 let equal_universe = Array.equal (Array.equal Char.equal)
-
-(* The most inneficient array manipulation in the universe, but it will get the job done. *)
-let expand universe =
-  let empty_row = Array.for_all ~f:(Char.equal '.') in
-  let empty_col universe col =
-    0 --^ Array.length universe
-    |> Seq.for_all (fun row -> Char.equal '.' universe.(row).(col))
-  in
-  let add_universe_col universe col =
-    let add_row_col row =
-      let new_lenght = Array.length row + 1 in
-      let new_row = Array.init new_lenght ~f:(Fun.const '.') in
-      0 -- col |> Seq.iter (fun i -> new_row.(i) <- row.(i));
-      col --^ Array.length row |> Seq.iter (fun i -> new_row.(i + 1) <- row.(i));
-      new_row
-    in
-    Array.map ~f:add_row_col universe
-  in
-  let universe_expanded_columns =
-    0 --^ Array.length universe.(0)
-    |> Seq.to_rev_list
-    |> List.fold_left
-         ~f:(fun current_universe col ->
-           if empty_col universe col then add_universe_col current_universe col
-           else current_universe)
-         ~init:universe
-  in
-  Array.flat_map
-    ~f:(fun row -> if empty_row row then [| row; row |] else [| row |])
-    universe_expanded_columns
-
-let%test "universe expansion" =
-  let contracted = parse sample in
-  let expanded = expand contracted in
-  let expected = parse expanded_universe in
-  Test.(run bool (equal_universe expanded expected) ~expect:true)
 
 let list_planets universe =
   Array.foldi
@@ -85,37 +55,76 @@ let list_planets universe =
         ~init:acc)
     ~init:[] universe
 
-let%test "correct number of planets" =
-  let planets = list_planets (parse sample |> expand) in
-  Test.(run int (List.length planets) ~expect:9)
-
-let%test "correct coordinates for planets" =
-  let planets = list_planets (parse sample |> expand) in
-  let has_first_planets =
-    List.mem ~eq:(fun (a, b) (c, d) -> a = c && b = d) (0, 4) planets
+let create_universe grid =
+  let is_empty_row = Array.for_all ~f:(Char.equal '.') in
+  let is_empty_col col =
+    0 --^ Array.length grid
+    |> Seq.for_all (fun row -> Char.equal '.' grid.(row).(col))
   in
-  Test.(run bool has_first_planets ~expect:true)
+  let planets = list_planets grid in
+  let empty_row =
+    Array.mapi ~f:(fun i row -> if is_empty_row row then Some i else None) grid
+    |> Array.to_list |> List.keep_some
+  in
+  let empty_col =
+    0 --^ Array.length grid.(0)
+    |> Seq.map (fun col -> if is_empty_col col then Some col else None)
+    |> Seq.to_list |> List.keep_some
+  in
+  { planets; empty_row; empty_col }
 
 let manhattan_distance (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
 
-module Part_1 = struct
-  let solve input =
-    let universe = parse input in
-    let universe = expand universe in
-    let planets = list_planets universe in
-    let double_distances =
-      List.flat_map
-        ~f:(fun p1 -> List.map ~f:(fun p2 -> manhattan_distance p1 p2) planets)
-        planets
-    in
-    Util.sum double_distances / 2
+let distance expansion_factor universe p1 p2 =
+  let points_in_between a b xs =
+    let lower = min a b in
+    let upper = max a b in
+    List.filter ~f:(fun x -> x > lower && x < upper) xs |> List.length
+  in
+  let manhattan_distance = manhattan_distance p1 p2 in
+  let x1, y1 = p1 in
+  let x2, y2 = p2 in
+  let empty_cols = points_in_between y1 y2 universe.empty_col in
+  let empty_rows = points_in_between x1 x2 universe.empty_row in
+  manhattan_distance - empty_rows - empty_cols
+  + ((empty_cols + empty_rows) * expansion_factor)
 
+let%test "correct number of planets" =
+  let planets = list_planets (parse sample) in
+  Test.(run int (List.length planets) ~expect:9)
+
+let%test "correct coordinates for planets" =
+  let planets = list_planets (parse sample) in
+  let has_first_planets =
+    List.mem ~eq:(fun (a, b) (c, d) -> a = c && b = d) (0, 3) planets
+  in
+  Test.(run bool has_first_planets ~expect:true)
+
+let solve_with_factor expansion_factor input =
+  let universe = parse input |> create_universe in
+  let double_distances =
+    List.flat_map
+      ~f:(fun p1 ->
+        List.map
+          ~f:(fun p2 -> distance expansion_factor universe p1 p2)
+          universe.planets)
+      universe.planets
+  in
+  Util.sum double_distances / 2
+
+module Part_1 = struct
+  let solve = solve_with_factor 2
   let%test "sample data" = Test.(run int (solve sample) ~expect:374)
 end
 
 module Part_2 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let solve = solve_with_factor 1_000_000
+
+  let%test "sample data 100x" =
+    Test.(run int (solve_with_factor 100 sample) ~expect:8410)
+
+  let%test "sample data 10x" =
+    Test.(run int (solve_with_factor 10 sample) ~expect:1030)
 end
 
 let run_1 () =
@@ -123,5 +132,5 @@ let run_1 () =
   ()
 
 let run_2 () =
-  (* Run.solve_int (module Part_2); *)
+  Run.solve_int (module Part_2);
   ()
