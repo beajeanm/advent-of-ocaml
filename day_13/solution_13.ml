fba2f6f832746ff962db67abbc5d23e6e2a5f19b
Jean-Michel Bea <jean-michel@opengamma.com>
2023-12-13 22:09:53 +0000

    Solve day 13 part 2

--- day_13/solution_13.ml
+++ day_13/solution_13.ml
@@ -45,62 +45,88 @@ let pattern_2 =
 |}
   |> String.trim
 
-let find_vertical_reflexion grid =
-  let is_reflexion row left_col =
+let other_sample =
+  {|
+...
+..#
+...
+
+...
+...
+.#.
+
+...
+#..
+...
+
+.#.
+...
+...
+
+...
+.#.
+...|}
+  |> String.trim
+
+let compare c1 c2 = if Char.equal c1 c2 then 0 else 1
+
+let find_vertical_reflexion allowed_mismatches grid =
+  let is_almost_reflexion allowed_mismatches row left_col =
     let bound = min (Array.length row - left_col - 2) left_col in
-    let all_match =
+    let mismatches =
       0 -- bound
       |> Seq.map (fun offset ->
-             abs
-             @@ Char.compare row.(left_col - offset) row.(left_col + 1 + offset))
+             compare row.(left_col - offset) row.(left_col + 1 + offset))
       |> Seq.fold_left Int.add 0
     in
-    all_match = 0
+    mismatches = allowed_mismatches
   in
-  let all_reflexions =
-    Array.map
-      ~f:(fun row ->
-        0 --^ (Array.length row - 1)
-        |> Seq.filter (is_reflexion row)
-        |> Seq.to_list)
-      grid
-    |> Array.to_list
+  let is_reflexion = is_almost_reflexion 0 in
+  let reflexion_for_row row_index =
+    0 --^ (Array.length grid.(row_index) - 1)
+    |> Seq.filter (is_almost_reflexion allowed_mismatches grid.(row_index))
   in
-  let first_row = List.hd all_reflexions in
-  List.fold_left ~f:(List.inter ~eq:Int.equal) ~init:first_row all_reflexions
-  (* We assume there is only one. *)
-  |> List.head_opt
-  (*Arrays are 0 indexed. *)
+  let valid_for_other_rows row_index left_col =
+    0 --^ Array.length grid
+    |> Seq.filter (Fun.negate @@ Int.equal row_index)
+    |> Seq.for_all (fun row -> is_reflexion grid.(row) left_col)
+  in
+  0 --^ Array.length grid
+  |> Seq.flat_map (fun row ->
+         reflexion_for_row row
+         |> Seq.filter (fun col -> valid_for_other_rows row col))
+  |> Seq.head
   |> Option.map (Int.add 1)
   |> Option.value ~default:0
 
-let find_horizontal_reflexion grid =
-  let is_reflexion col_index up_row =
+let find_horizontal_reflexion allowed_mismatches grid =
+  let is_almost_reflexion allowed_mismatches col_index up_row =
     let bound = min (Array.length grid - up_row - 2) up_row in
-    let all_match =
+    let mismatches =
       0 -- bound
       |> Seq.map (fun offset ->
-             abs
-             @@ Char.compare
-                  grid.(up_row - offset).(col_index)
-                  grid.(up_row + 1 + offset).(col_index))
+             compare
+               grid.(up_row - offset).(col_index)
+               grid.(up_row + 1 + offset).(col_index))
       |> Seq.fold_left Int.add 0
     in
-    all_match = 0
+    mismatches = allowed_mismatches
   in
-  let all_reflexions =
+  let is_reflexion = is_almost_reflexion 0 in
+  let reflexions_for_col col_index =
+    0 --^ (Array.length grid - 1)
+    |> Seq.filter (is_almost_reflexion allowed_mismatches col_index)
+  in
+  let valid_for_other_cols col_index up_row =
     0 --^ Array.length grid.(0)
-    |> Seq.map (fun col ->
-           0 --^ (Array.length grid - 1)
-           |> Seq.filter (is_reflexion col)
-           |> Seq.to_list)
-    |> Seq.to_list
+    |> Seq.filter (Fun.negate @@ Int.equal col_index)
+    |> Seq.for_all (fun col -> is_reflexion col up_row)
   in
-  let first_col = List.hd all_reflexions in
-  List.fold_left ~f:(List.inter ~eq:Int.equal) ~init:first_col all_reflexions
-  (* We assume there is only one. *)
-  |> List.head_opt
-  (*Arrays are 0 indexed. *)
+  0 --^ Array.length grid.(0)
+  |> Seq.flat_map (fun col ->
+         reflexions_for_col col
+         |> Seq.filter (fun row -> valid_for_other_cols col row))
+  |> Seq.head
   |> Option.map (Int.add 1)
   |> Option.map (Int.mul 100)
   |> Option.value ~default:0
@@ -126,24 +152,30 @@ let parse input =
   |> List.map ~f:(fun l -> Array.of_list l)
 
 let%test "test vertical reflexion" =
-  Test.(run int (find_vertical_reflexion (to_grid pattern_1)) ~expect:5)
+  Test.(run int (find_vertical_reflexion 0 (to_grid pattern_1)) ~expect:5)
 
 let%test "test vertical reflexion" =
-  Test.(run int (find_horizontal_reflexion (to_grid pattern_2)) ~expect:400)
+  Test.(run int (find_horizontal_reflexion 0 (to_grid pattern_2)) ~expect:400)
 
 module Part_1 = struct
   let solve input =
     let parsed = parse input in
-    let horizontals = List.map ~f:find_horizontal_reflexion parsed in
-    let verticals = List.map ~f:find_vertical_reflexion parsed in
+    let horizontals = List.map ~f:(find_horizontal_reflexion 0) parsed in
+    let verticals = List.map ~f:(find_vertical_reflexion 0) parsed in
     Util.sum horizontals + Util.sum verticals
 
   let%test "sample data" = Test.(run int (solve sample) ~expect:405)
+  let%test "sample data" = Test.(run int (solve other_sample) ~expect:303)
 end
 
 module Part_2 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let solve input =
+    let parsed = parse input in
+    let horizontals = List.map ~f:(find_horizontal_reflexion 1) parsed in
+    let verticals = List.map ~f:(find_vertical_reflexion 1) parsed in
+    Util.sum horizontals + Util.sum verticals
+
+  let%test "sample data" = Test.(run int (solve sample) ~expect:400)
 end
 
 let run_1 () =
@@ -151,5 +183,5 @@ let run_1 () =
   ()
 
 let run_2 () =
-  (* Run.solve_int (module Part_2); *)
+  Run.solve_int (module Part_2);
   ()
