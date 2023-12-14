b88a89a46c2ac50d0bbd1a51eca131e577ffcd1d
Jean-Michel Bea <jean-michel@opengamma.com>
2023-12-14 13:36:15 +0000

    Solve day 14 part 2

--- day_14/solution_14.ml
+++ day_14/solution_14.ml
@@ -16,6 +16,27 @@ O.#..O.#.#
 
 type rock = Round | Square | Empty [@@deriving eq, show { with_path = false }]
 
+let show_rock = function Round -> "O" | Square -> "#" | Empty -> "."
+let hash_rock = function Round -> 7 | Square -> 23 | Empty -> 59
+
+module Platform = struct
+  type t = rock Array.t Array.t [@@deriving eq]
+
+  let show_platform platform =
+    Array.map
+      ~f:(fun row ->
+        Array.map ~f:show_rock row |> Seq.of_array |> String.concat_seq ~sep:" ")
+      platform
+    |> Seq.of_array
+    |> String.concat_seq ~sep:"\n"
+
+  let hash (p : t) =
+    Array.fold_left
+      ~f:(fun hash ->
+        Array.fold_left ~f:(fun hash rock -> hash + hash_rock rock) ~init:hash)
+      ~init:0 p
+end
+
 let parse input =
   let parse_rock = function
     | 'O' -> Round
@@ -26,9 +47,6 @@ let parse input =
   let parse_line line = String.to_array line |> Array.map ~f:parse_rock in
   String.lines_seq input |> Seq.map parse_line |> Seq.to_array
 
-let%test "parse input" =
-  Test.(run int (parse sample |> Array.length) ~expect:10)
-
 let transpose grid =
   let width = Array.length grid.(0) in
   let height = Array.length grid in
@@ -38,7 +56,18 @@ let transpose grid =
          0 --^ width |> Seq.iter (fun j -> transpose.(j).(i) <- grid.(i).(j)));
   transpose
 
-let migrate_north grid =
+let flip platform =
+  let flip_row row =
+    let len = Array.length row in
+    0 --^ (len / 2)
+    |> Seq.iter (fun i ->
+           let tmp = row.(i) in
+           row.(i) <- row.(len - 1 - i);
+           row.(len - 1 - i) <- tmp)
+  in
+  Array.iter ~f:flip_row platform
+
+let sort platform =
   let rec sort_row i j row =
     if i < 0 then ()
     else
@@ -53,34 +82,86 @@ let migrate_north grid =
           sort_row (i - 1) (j - 1) row
       | _ -> sort_row (i - 1) (j - 1) row
   in
-  let sort platform =
-    let i = Array.length platform.(0) - 2 in
-    let j = Array.length platform.(0) - 1 in
-    Array.iter ~f:(sort_row i j) platform
-  in
+  let i = Array.length platform.(0) - 2 in
+  let j = Array.length platform.(0) - 1 in
+  Array.iter ~f:(sort_row i j) platform
 
-  let t = transpose grid in
+let migrate_north platform =
+  let t = transpose platform in
   sort t;
   transpose t
+
+let migrate_west platform =
+  sort platform;
+  platform
+
+let migrate_east platform =
+  flip platform;
+  sort platform;
+  flip platform;
+  platform
+
+let migrate_south platform =
+  let t = transpose platform in
+  flip t;
+  sort t;
+  flip t;
+  transpose t
+
+let%test "parse input" =
+  Test.(run int (parse sample |> Array.length) ~expect:10)
+
+let load platform =
+  let len = Array.length platform in
+  let count_rocks row =
+    Array.filter ~f:(equal_rock Round) row |> Array.length
+  in
+  Array.foldi
+    ~f:(fun acc index row -> acc + ((len - index) * count_rocks row))
+    ~init:0 platform
+
+let spin platform =
+  migrate_north platform |> migrate_west |> migrate_south |> migrate_east
+
+module PlatformMap = Hashtbl.Make (Platform)
+
+let spin' spins platform =
+  (* Save us from infinite loops *)
+  let max_iteration = 200 in
+  let cache = PlatformMap.create max_iteration in
+  let rec aux iteration platform =
+    if iteration = max_iteration then failwith "Not cycle found"
+    else
+      let platform = spin platform in
+      let prev = PlatformMap.find_opt cache platform in
+      if Option.is_some prev then
+        let prev = Option.get_exn_or "" prev in
+        (prev, iteration - prev)
+      else (
+        PlatformMap.add cache platform iteration;
+        aux (iteration + 1) platform)
+  in
+  PlatformMap.add cache platform 0;
+  let start_cyle, cycle_len = aux 1 platform in
+  0 --^ (((spins - start_cyle) mod cycle_len) + start_cyle)
+  |> Seq.fold_left (fun p _ -> spin p) platform
 
 module Part_1 = struct
   let solve input =
     let platform = parse input in
     let platform = migrate_north platform in
-    let len = Array.length platform in
-    let count_rocks row =
-      Array.filter ~f:(equal_rock Round) row |> Array.length
-    in
-    Array.foldi
-      ~f:(fun acc index row -> acc + ((len - index) * count_rocks row))
-      ~init:0 platform
+    load platform
 
   let%test "sample data" = Test.(run int (solve sample) ~expect:136)
 end
 
 module Part_2 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let solve input =
+    let platform = parse input in
+    let spins = 1000000000 in
+    spin' spins platform |> load
+
+  let%test "sample data" = Test.(run int (solve sample) ~expect:64)
 end
 
 let run_1 () =
@@ -88,5 +169,5 @@ let run_1 () =
   ()
 
 let run_2 () =
-  (* Run.solve_int (module Part_2); *)
+  Run.solve_int (module Part_2);
   ()
