7525ba5e49c35bb4b1e58bd16d62b420ef7fc467
Jean-Michel Bea <jean-michel@opengamma.com>
2023-12-19 00:18:10 +0000

    Solve day 12 part 2 (finally!)

--- day_12/solution_12.ml
+++ day_12/solution_12.ml
@@ -11,48 +11,62 @@ let sample =
 |}
   |> String.trim
 
-let count_contiguous row =
-  let grouped = List.group_succ ~eq:Char.equal row in
-  List.filter ~f:(fun cs -> Char.equal '#' (List.hd cs)) grouped
-  |> List.map ~f:List.length
+let count0 count (row, counts) =
+  let contains = List.mem ~eq:Char.equal in
+  if List.is_empty row then if List.is_empty counts then 1 else 0
+  else if List.is_empty counts then if contains '#' row then 0 else 1
+  else
+    let first_char = List.hd row in
+    let current_count = List.hd counts in
+    let good_count =
+      if Char.(equal '.' first_char || equal '?' first_char) then
+        count (List.tl row, counts)
+      else 0
+    in
+    let broken_count =
+      if Char.(equal '#' first_char || equal '?' first_char) then
+        if
+          List.length row > current_count
+          && (not (List.take current_count row |> contains '.'))
+          && contains (List.nth row current_count) [ '.'; '?' ]
+        then count (List.drop (current_count + 1) row, List.tl counts)
+        else 0
+      else 0
+    in
+    good_count + broken_count
+
+module Key = struct
+  type t = char list * int list [@@deriving eq]
+
+  let hash = CCHash.(pair (list char) (list int))
+end
+
+module KeyMap = Hashtbl.Make (Key)
+
+let cached_count row counts =
+  (* Add an operational spring at the end to simplify some edge cases. *)
+  let row = List.concat [ row; [ '.' ] ] in
+  let cache = CCCache.unbounded ~eq:Key.equal ~hash:Key.hash 10000 in
+  CCCache.with_cache_rec cache count0 (row, counts)
 
 let%test "Count broken groups" =
   Test.(
-    run (list int)
-      (count_contiguous ("#.#.###" |> String.to_list))
-      ~expect:[ 1; 1; 3 ])
+    run int (cached_count ("#.#.###." |> String.to_list) [ 1; 1; 3 ]) ~expect:1)
 
 let%test "Count broken groups 2" =
   Test.(
-    run (list int)
-      (count_contiguous ("#....######..#####." |> String.to_list))
-      ~expect:[ 1; 6; 5 ])
-
-let arrangements row =
-  let rec aux prefix = function
-    | [] -> [ List.rev prefix ]
-    | '?' :: cs ->
-        List.concat [ aux ('#' :: prefix) cs; aux ('.' :: prefix) cs ]
-    | c :: cs -> aux (c :: prefix) cs
-  in
-  aux [] row
-
-let matching_arrangements row counts =
-  let eq = List.equal Int.equal in
-  arrangements row
-  |> List.filter ~f:(fun a -> eq counts (count_contiguous a))
-  |> List.length
+    run int
+      (cached_count ("#....######..#####." |> String.to_list) [ 1; 6; 5 ])
+      ~expect:1)
 
 let%test "counting arrangements 1" =
   Test.(
-    run int
-      (matching_arrangements ("???.###" |> String.to_list) [ 1; 1; 3 ])
-      ~expect:1)
+    run int (cached_count ("???.###." |> String.to_list) [ 1; 1; 3 ]) ~expect:1)
 
 let%test "counting arrangements 3" =
   Test.(
     run int
-      (matching_arrangements ("?###????????" |> String.to_list) [ 3; 2; 1 ])
+      (cached_count ("?###????????." |> String.to_list) [ 3; 2; 1 ])
       ~expect:10)
 
 let parse input =
@@ -69,8 +83,7 @@ module Part_1 = struct
   let solve input =
     let parsed = parse input in
     List.map
-      ~f:(fun (row, counts) ->
-        matching_arrangements (String.to_list row) counts)
+      ~f:(fun (row, counts) -> cached_count (String.to_list row) counts)
       parsed
     |> Util.sum
 
@@ -78,8 +91,18 @@ module Part_1 = struct
 end
 
 module Part_2 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let solve input =
+    let unfold (row, counts) =
+      ( String.concat ~sep:"?" [ row; row; row; row; row ],
+        List.concat [ counts; counts; counts; counts; counts ] )
+    in
+    let parsed = parse input |> List.map ~f:unfold in
+    List.map
+      ~f:(fun (row, counts) -> cached_count (String.to_list row) counts)
+      parsed
+    |> Util.sum
+
+  let%test "sample data" = Test.(run int (solve sample) ~expect:525152)
 end
 
 let run_1 () =
@@ -87,5 +110,5 @@ let run_1 () =
   ()
 
 let run_2 () =
-  (* Run.solve_int (module Part_2); *)
+  Run.solve_int (module Part_2);
   ()
